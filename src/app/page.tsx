1cf8319081258fcc8e0f860d1de1433e1604b842
alysonbarreto02 <alysonbarretito2123@gmail.com>
2023-10-21 17:21:30 -0300

    feat: input component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,17 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <div className="w-full bg-gray-700 h-16"></div>;
+  return (
+    <div className="w-full h-screen justify-center items-center flex">
+      <div className="w-96">
+        <div className="w-full">
+          <label className="text-gray-500 font-medium">Nome</label>
+          <input
+            type="text"
+            className="h-12 border rounded-md w-full border-gray-300"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
