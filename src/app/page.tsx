c2069106f4cd48671c676aaceff88e51074a832c
Israel Sales <israelsales043@gmail.com>
2023-10-26 12:22:08 -0300

    feat: edit input-base component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,32 @@
-import { InputBase, Title1, Text, Title2, Header } from "@/app/components";
-import PhoneIcon from "@mui/icons-material/Phone";
-import DirectionsIcon from "@mui/icons-material/Directions";
+'use client';
+
+import { InputBase, Title1, Text, Title2, Header } from '@/app/components';
+import PhoneIcon from '@mui/icons-material/Phone';
+import DirectionsIcon from '@mui/icons-material/Directions';
+import { useForm } from 'react-hook-form';
 export default function Home() {
+  type registerForm = {
+    name: string;
+    email: string;
+  };
+
+  const {
+    register,
+    formState: { errors },
+    control,
+    handleSubmit,
+  } = useForm<registerForm>({
+    mode: 'onBlur',
+    defaultValues: {
+      email: '',
+      name: '',
+    },
+  });
+
+  const teste = (data: any) => {
+    console.log(data);
+  };
+
   return (
     <div className="w-full ">
       <Header />
@@ -14,19 +39,27 @@ export default function Home() {
             casa onde será muito bem acolhido!"
           />
         </div>
-        <form className="w-full border-2 shadow-lg border-gray-300 bg-white rounded-xl p-5 flex flex-col gap-3">
+        <form
+          onSubmit={handleSubmit(teste)}
+          className="w-full border-2 shadow-lg border-gray-300 bg-white rounded-xl p-5 flex flex-col gap-3"
+        >
           <Title2 title="Deixe a gente te conhecer melhor!" />
-          <InputBase />
-          <InputBase />
-          <InputBase />
-          <InputBase />
-          <InputBase />
-          <InputBase />
-          <InputBase />
-          <InputBase />
-          <InputBase />
-          <InputBase />
-          <InputBase />
+          <InputBase
+            control={control}
+            label="Seu nome"
+            name="name"
+            rules={{ required: 'Nome obrigatorio' }}
+            error={errors.name?.message}
+          />
+          <InputBase
+            control={control}
+            label="Seu email"
+            name="email"
+            rules={{ required: 'Email obrigatorio' }}
+            error={errors.email?.message}
+          />
+
+          <button type="submit">Enviar</button>
         </form>
         <div className="mt-6">
           <Title2 title="Contate-nos" />
